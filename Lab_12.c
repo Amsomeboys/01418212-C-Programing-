9c281f8e74f8fb4f1d44833296a708a301a9b62d
Amsomeboys <khnun1090@gmail.com>
2023-09-07 14:26:20 +0700

    update

--- Lab_12.c
+++ Lab_12.c
@@ -2,38 +2,51 @@
 
 main(){
 
-	int num1=0,num2,sum,end=1;
+	int num1=0,num2,sum;
 	int tmp = 1 ;
+	char con='y';
 
-	while(tmp == 1 && end == 1 )
+	while(tmp == 1 && con == 'y' )
 	{
-
-		printf("Enter first number (Enter -1 to stop program):");
-		scanf("%d",&num1);
-		while(num1 == -1 && end == 1)
+		while(con == 'N')
 		{
 			printf("\n\"End Program\"\n\n");
-			end=0;
+			con == 'y';
 		}
-		while(end != 0 && tmp == 1)
+
+		while(tmp == 1 )
+		{
+			printf("Enter first number:");
+			scanf("%d",&num1);
+			tmp++;
+		}
+
+		while(con != 'N' && tmp == 2)
 		{
 			printf("Enter second number :");
 			scanf("%d",&num2);
 			sum = num1 + num2;
-			while(tmp<=1&&sum>=52)
+
+			while(tmp<=2&&sum>=52)
 			{
 				printf("\n\"Summary Result is more than 52\"\n\n");
 				tmp++;
-
 			}
-			while(tmp<=1&&sum<52)
+			while(tmp<=2&&sum<52)
 			{
 				printf("\n\"Summary Result is not more than 52\" \n\n");
 				tmp++;
 
 			}
 		}
-		tmp = 1;
 
+		while(tmp == 3)
+		{
+			printf("Continue Program ? (y/N):");
+			scanf(" %c",&con);
+			tmp++;
+		}
+		tmp=1;
+		printf("\n\n");
 	}
 }
--- Lab_13.c
+++ Lab_13.c
@@ -1,71 +1,60 @@
 #include <stdio.h>
 
-main(){
-
-    int start,end,count;
+main()
+{//1
+    int start=0,end,count;
     int i;
-    int run = 0;
+    char run = 'y';
 
-
-
-    while(run == 0)
-    {
-    	printf("Please Input Start Number OR Enter 0 to stop program): ");
+    while(run == 'y' && start <= 0)
+    {//2
+    	printf("Please Input Start Number: ");
     	scanf("%d",&start);
-    	run++;
-
-    while( start == 0 )
-    {
-        printf("\n\"End Program\" \n\n");
-        break;
-    }
-
-    while(start >= 1 && run == 1)
-    {
-    	printf("\nPlease Input End Number : ");
-    	scanf("%d",&end);
-
-    while(start > end)
-    {
-        printf("\n\"Please enter start number more than end number\"");
-        start = 1;
-        end = 1;
-		run = 0;
-    }
-
-    while(start > 0 && end > 0 && run == 1)
-    {
-        printf("\nPrime numbers in this range is :");
-
-       while (start <= end)  // check start < end ?
-       {
-            i = 1;        // reset to default value every round
-            count = 0;
-
-
-         while (i <= start && count < 3) // check i < start number ? and mod == 0 count < 3
-         {
-            while (start % i == 0 && count < 3) //check mod == 0 and count < 3
-            {
-                count++;  //count +1
-                i++; // i+1
-            }
-            i++; // i+1
-        }
-            while (count == 2) //count == 2
-            {
-                printf("%d  ", start); // print
-                count = 0; // reset to out loop
-            }
-
-            start++; // start +1
-
-        }
-            end = 0;
-        }
-       printf("\n\n");
-        start = 1;
-        run = 0;
-}
-}
-}
+    	while(start <=  0)
+    	{//3
+			printf("\n\"Enter more than 0\"");
+			break;
+		}//3
+    	while( run == 'N' )
+    	{//4
+        	printf("\n\"End Program\" \n\n");
+        	break;
+    	}//4
+    	while(start > 0 && run == 'y')
+    	{//5
+    		printf("\nPlease Input End Number : ");
+    		scanf("%d",&end);
+    		while(start > end)
+    		{//6
+        		printf("\n\"Please enter start number more than end number\"");
+        		start = 1;
+        		end = 1;
+    		}//6
+    		while(start > 0 && end > 0 )
+    		{//7
+        		printf("\nPrime numbers in this range is :");
+       			while (start <= end)  // check start < end ?
+       			{//8
+            		i = 1;        // reset to default value every round
+            		count = 0;
+         			while (i <= start && count < 3) // check i < start number ? and mod == 0 count < 3
+         			{//9
+            			while (start % i == 0 && count < 3) //check mod == 0 and count < 3
+            			{//10
+                			count++;  //count +1
+                			i++; // i+1
+            			}//10
+            			i++; // i+1
+        			}//9
+            		while (count == 2) //count == 2
+            		{//11
+                		printf("%d  ", start); // print
+                		count = 0; // reset to out loop
+            		}//11
+            		start++; // start +1
+        		}//8
+            	end = 0;
+        	}
+		}
+	}//2
+}//1
