8167f18c4ddf86db565f90390f6f14c2cba4ddf1
Amsomeboys <khnun1090@gmail.com>
2023-09-21 11:35:58 +0700

    Update Lab_29.c

--- Lab_29.c
+++ Lab_29.c
@@ -2,34 +2,56 @@
 
 main()
 {
-    int first1D[10], second1D[10], third1D[10], i, size = 10;
+    int first1D[10], second1D[10], third1D[10], i, size = 10, run = 0;
+    char finish = 'y';
 
-    for (i = 0; i < size; i++)
+    while (finish == 'y')
     {
-        first1D[i] = 0;
-        second1D[i] = 0;
-    }
+        for (i = 0; i < size; i++)
+        {
+            first1D[i] = 0;
+            second1D[i] = 0;
+        }
 
-    printf("Enter number in first array (10 number)");
-    for (i = 0; i < size; i++)
-    {
-        printf("\nFirst[%d] : ", i);
-        scanf("%d", &first1D[i]);
-    }
-    printf("Enter number in second array (10 number)");
-    for (i = 0; i < size; i++)
-    {
-        printf("\nSecond[%d] : ", i);
-        scanf("%d", &second1D[i]);
-    }
+        printf("Enter number in first array (10 number)");
+        for (i = 0; i < size; i++)
+        {
+            printf("\nFirst[%d] : ", i);
+            scanf("%d", &first1D[i]);
+        }
+        printf("Enter number in second array (10 number)");
+        for (i = 0; i < size; i++)
+        {
+            printf("\nSecond[%d] : ", i);
+            scanf("%d", &second1D[i]);
+        }
 
-    for (i = 0; i < size; i++)
-    {
-        third1D[i] = first1D[i] + second1D[i];
-    }
-    printf("Sum Array Result is : ");
-    for (i = 0; i < size; i++)
-    {
-        printf(" %d ", third1D[i]);
+        for (i = 0; i < size; i++)
+        {
+            third1D[i] = first1D[i] + second1D[i];
+        }
+        printf("Sum Array Result is : ");
+        for (i = 0; i < size; i++)
+        {
+            printf(" %d ", third1D[i]);
+        }
+
+        while (run == 0)
+        {
+            printf("\n\nContinue Program ? (y/N) : ");
+            scanf(" %c", &finish);
+            if (finish == 'y' || finish == 'N')
+            {
+                run = 1;
+            }
+            else
+            {
+                printf("Enter only \" y \" or \"N\"");
+            }
+        }
+        if (finish == 'N')
+        {
+            printf("\"End Program\"");
+        }
     }
 }
