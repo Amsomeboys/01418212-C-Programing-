914de635d8beaf87a74da8b3551283bc7e30f74e
Amsomeboys <khnun1090@gmail.com>
2023-09-28 11:19:20 +0700

    Update Lab_27.c

--- Lab_27.c
+++ Lab_27.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+int factorial(int n);
+
 main()
 {
+    int num, result_1, result_2, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // use func
+    result_1 = factorial(num);
+
+    result_2 = 1;
+    // not use func
+    for (i = 1; i <= num; i++)
+    {
+        result_2 = result_2 * i;
+    }
+
+    // with func
+    printf("Factorial of %d is %d\n", num, result_1);
+
+    // without func
+    printf("Factorial of %d is %d\n", num, result_2);
+}
+
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
 }
