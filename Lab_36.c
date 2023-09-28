b70fa8523b7d652d2a6f3ace0f29c53c13a17f33
Amsomeboys <khnun1090@gmail.com>
2023-09-28 07:29:02 +0000

    Update_Lab_36

--- Lab_36.c
+++ Lab_36.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
-main() {}
+main() 
+{
+    float arr[20]; 
+    float *index,avg;
+    int i;
+
+    printf("Enter number in Array\n");
+    for(i=0;i<20;i++)
+    {
+        printf("Arr[%d] : ",i);
+        scanf("%f",&arr[i]);
+    }
+    for(i=0;i<20;i++)
+    {
+        index = &arr[i];
+        avg += *index;
+    }
+        avg = avg/20;
+        printf("\n avg : %.2f",avg);
+}
