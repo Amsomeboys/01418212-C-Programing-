e7681214bf48b600ac229072ebd33828854aa0ea
Amsomeboys <khnun1090@gmail.com>
2023-10-03 12:37:43 +0700

    Update

--- Lab_37.c
+++ Lab_37.c
@@ -2,54 +2,68 @@
 main()
 {
     float arr[3][3][3], *p, sum;
-    int i, j, k, size = 2;
+    int i, j, k, size = 3, run;
+    char finish = 'y';
 
-    for (i = 0; i < size; i++)
+    while (finish == 'y')
     {
-        for (j = 0; j < size; j++)
+        for (i = 0; i < size; i++)
         {
-            for (k = 0; k < size; k++)
+            for (j = 0; j < size; j++)
             {
-                arr[i][j][k] = 0;
+                for (k = 0; k < size; k++)
+                {
+                    arr[i][j][k] = 0;
+                }
             }
         }
-    }
 
-    printf("Enter number in Array3D\n\n");
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
+        printf("Enter number in Array3D\n\n");
+        for (i = 0; i < size; i++)
         {
-            for (k = 0; k < size; k++)
+            for (j = 0; j < size; j++)
             {
-                printf("Arr[%d][%d][%d] : ", i, j, k);
-                scanf("%f", &arr[i][j][k]);
+                for (k = 0; k < size; k++)
+                {
+                    printf("Arr[%d][%d][%d] : ", i, j, k);
+                    scanf("%f", &arr[i][j][k]);
+                }
             }
         }
-    }
-    printf("\n\n");
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
+        printf("\n\n");
+        for (i = 0; i < size; i++)
         {
-            for (k = 0; k < size; k++)
+            for (j = 0; j < size; j++)
             {
-                p = &arr[i][j][k];
-                sum += *p;
-                printf("%u : %.2f\n", p, *p);
+                for (k = 0; k < size; k++)
+                {
+                    p = &arr[i][j][k];
+                    sum += *p;
+                    printf("%u : %.2f\n", p, *p);
+                }
             }
         }
+        printf("\nSum of array by pointer is :%.2f", sum);
+        run = 1;
+
+        while (run == 1)
+        {
+            printf("\n\ncontinue Program? (y/N): ");
+            scanf(" %c", &finish);
+            if (finish == 'y' || finish == 'N')
+            {
+                run = 0;
+                p = 0;
+                sum = 0;
+            }
+            else
+            {
+                printf("Enter only \" y \" or \"N\"");
+            }
+        }
+        if (finish == 'N')
+        {
+            printf("\"End Program\"\n");
+        }
     }
-printf("%f",sum);
-    // for (i = 0; i < size; i++)
-    // {
-    //     for (j = 0; j < size; j++)
-    //     {
-    //         for (k = 0; k < size; k++)
-    //         {
-    //             sum += *(p + i);
-    //             printf("\nsum %.2f", sum);
-    //         }
-    //     }
-    // }
 }
--- Lab_38.c
+++ Lab_38.c
@@ -1,2 +1,43 @@
 #include <stdio.h>
-main() {arr1D[3],arr2D[3][3],arr3D[3][3][3];}
+main()
+{
+    int arr1D[3], arr2D[3][3], arr3D[3][3][3], *p1, *p2, p3, size = 3, i, j, k;
+    p1 = &arr1D[0];
+    p2 = &arr2D[0][0];
+    p3 = &arr3D[0][0][0];
+    for (i = 0; i < size; i++)
+    {
+        arr1D[i] = 0;
+        for (j = 0; j < size; j++)
+        {
+            arr2D[i][j] = 0;
+            for (k = 0; k < size; k++)
+            {
+                arr3D[i][j][k] = 0;
+            }
+        }
+    }
+
+    for (i = 0; i < size; i++)
+    {
+
+        printf("\nArr1D[%d] : %u", i, p1 + i);
+    }
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            printf("\nArr2D[%d][%d] : %u", i, j, p2);
+        }
+    }
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            for (k = 0; k < size; k++)
+            {
+                printf("\nArr3D[%d][%d][%d] : %u", i, j, k, p3);
+            }
+        }
+    }
+}
